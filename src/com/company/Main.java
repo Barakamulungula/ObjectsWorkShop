0df74c2ebba7f247ec1b58e0fa00ce8898931163
Baraka Mulungula <barakamulungula@Interapts-MacBook-Pro-2.local>
2018-02-01 13:10:52 -0500

    Full player class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+       Player p = new Player();
+       p.getAllInfo();
+       p.setName("Lionel Messi");
+       p.setNumber(10);
+       p.setPosition("Right wing");
+       p.setSalary(2500000);
+       p.getAllInfo();
+
+
     }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,58 @@
 package com.company;
 
 public class Player {
+
+    private String name;
+    private int number;
+    private String position;
+    private float salary;
+
+    public Player(){
+        setName("Cristiano Ronaldo");
+        setNumber(7);
+        setPosition("Left wing");
+        setSalary(3000000);
+    }
+
+    public void getAllInfo(){
+        System.out.println("Name: "+getName());
+        System.out.println("Number: "+getNumber());
+        System.out.println("Position: "+getPosition());
+        System.out.println("Salary: $"+getSalary()+"\n");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+
+
 }
